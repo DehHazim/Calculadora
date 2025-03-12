e786dfefba3743440bcc220d21592d2a004cccbc
Deborah Bello <118914158+DehHazim@users.noreply.github.com>
2025-03-12 17:51:39 -0300

    Create calculadora.cs
    Inseri as primeiras funções ao projeto.

---
+++ calculadora.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+
+  static double Add(double num1, double num2) => num1 + num2;
+  static double Sub(double num1, double num2) => num1 - num2; 
+  static double Mult(double num1, double num2) => num1 * num2;
+  static double Divi(double num1, double num2)
+  
+}
